0380f14c88f3fe8c2877e3dc015d9ccf87e97100
Luca Di Stefano <luca.di.stefano@tuwien.ac.at>
2024-08-22 12:09:00 +0200

    refactor: refactor common logic in NuXmvInt

--- pyxmv/nuxmvint.py
+++ pyxmv/nuxmvint.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 import pexpect
 from shutil import which
+from functools import wraps
 from .simulation_heuristics import UserChoice
 
 
@@ -35,7 +36,12 @@ class NuXmvInt:
         if which("nuxmv") is None:
             raise FileNotFoundError("nuxmv not in PATH")
         self.nuxmv = pexpect.spawn("nuxmv", ["-int"], encoding="utf-8")
+        self.nuxmv.setecho(False)
         self.expect_prompt()
+
+    def __del__(self):
+        if self.nuxmv is not None:
+            self.nuxmv.kill(9)
 
     def expect_prompt(self, timeout: int | None = None) -> int:
         try:
@@ -61,9 +67,15 @@ class NuXmvInt:
         PyXmvError.factory(self.nuxmv.before)
         return self.nuxmv.before
 
-    def __del__(self):
-        if self.nuxmv is not None:
-            self.nuxmv.kill(9)
+    @staticmethod
+    def nuxmv_cmd(func) -> str:
+        @wraps(func)
+        def wrapper(self, *args, **kwargs):
+            cmd, timeout = func(self, *args, **kwargs)
+            self.nuxmv.sendline(cmd)
+            self.nuxmv.expect_exact(cmd)
+            return self.get_output(timeout)
+        return wrapper
 
     def msat_setup(self, fname: Path, shown_states: int = 65535) -> None:
         cmds = (
@@ -76,7 +88,7 @@ class NuXmvInt:
             self.expect_prompt()
             PyXmvError.factory(self.nuxmv.before)
 
-    def init(self, h, c: str | None = "TRUE", timeout: int | None = None):
+    def init(self, h, c: str | None = "TRUE", timeout: int | None = None) -> str:  # noqa: E501
         self.nuxmv.sendline(f"""msat_pick_state -c "{c}" -v -i""")
         output = self.get_output(timeout=timeout, prompts=[
             r"Choose a state from the above \(0-[0-9]+\): ",
@@ -87,20 +99,20 @@ class NuXmvInt:
         self.nuxmv.sendline(str(choice))
         return self.get_output(timeout)
 
-    def ic3(self, ltlspec: str | None = None, timeout: int | None = None) -> str:  # noqa: E501
+    @nuxmv_cmd
+    def ic3(self, ltlspec: str | None = None, timeout: int | None = None) -> tuple[str, int | None]:  # noqa: E501
         ltlspec = f"""-p "{ltlspec}" """ if ltlspec else ""
-        self.nuxmv.sendline(f"check_ltlspec_ic3 {ltlspec}")
-        return self.get_output(timeout)
+        return (f"check_ltlspec_ic3 {ltlspec}"), timeout
 
-    def ic3_invar(self, ltlspec: str | None = None, timeout: int | None = None) -> str:  # noqa: E501
+    @nuxmv_cmd
+    def ic3_invar(self, ltlspec: str | None = None, timeout: int | None = None) -> tuple[str, int | None]:  # noqa: E501
         ltlspec = f"""-L "{ltlspec}" """ if ltlspec else ""
-        self.nuxmv.sendline(f"check_property_as_invar_ic3 {ltlspec}")
-        return self.get_output(timeout)
+        return f"check_property_as_invar_ic3 {ltlspec}", timeout
 
-    def bmc(self, bound: int, ltlspec: str | None = None, timeout: int | None = None) -> str:  # noqa: E501
+    @nuxmv_cmd
+    def bmc(self, bound: int, ltlspec: str | None = None, timeout: int | None = None) -> tuple[str, int | None]:  # noqa: E501
         ltlspec = f"""-p "{ltlspec}" """ if ltlspec else ""
-        self.nuxmv.sendline(f"msat_check_ltlspec-bmc -k {bound} {ltlspec}")
-        return self.get_output(timeout)
+        return f"msat_check_ltlspec-bmc -k {bound} {ltlspec}", timeout
 
     def reset(self) -> None:
         self.nuxmv.sendline("reset")
@@ -114,7 +126,7 @@ class NuXmvInt:
         print(self.nuxmv.before)
         return self.nuxmv.before.split(NuXmvInt.STATE_SEP)[1:]
 
-    def simulate(self, steps=1, c: str = "TRUE", heuristic=None) -> None:  # noqa: E501
+    def simulate(self, steps=1, c: str = "TRUE", heuristic=None) -> None:
         h = UserChoice() if heuristic is None else heuristic
         for _ in range(steps):
             states = self.get_successor_states(c)
